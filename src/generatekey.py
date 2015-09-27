ae4edb728c7e37bbc4dac1be797adf5c1d234fa0
Anton Relin <arelin@sas.upenn.edu>
2015-09-27 08:07:58 +0000

    forgot to import socket

--- src/generatekey.py
+++ src/generatekey.py
@@ -12,6 +12,7 @@ from Tkinter import Tk, RIGHT, BOTH, RAISED, Text, END, Toplevel, Label, Entry
 from ttk import Frame, Button, Style
 import os
 import gnupg
+import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 3000
