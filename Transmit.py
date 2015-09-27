937de79a31452ae819e5a46d406040d2c7086540
Frank Cash <cfcash@mtu.edu>
2015-09-27 04:48:40 -0400

    Adds RX

--- Transmit.py
+++ Transmit.py
@@ -4,7 +4,7 @@
 # Title: Transmit
 # Author: AcidSloth
 # Description: Transmits GPG
-# Generated: Sat Sep 26 02:16:18 2015
+# Generated: Sun Sep 27 01:52:37 2015
 ##################################################
 
 if __name__ == '__main__':
@@ -22,7 +22,6 @@ from gnuradio import audio
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
-from gnuradio import uhd
 from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
@@ -30,6 +29,7 @@ from gnuradio.wxgui import forms
 from gnuradio.wxgui import scopesink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import osmosdr
 import time
 import wx
 
@@ -51,6 +51,44 @@ class Transmit(grc_wxgui.top_block_gui):
         ##################################################
         # Blocks
         ##################################################
+        self._freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	value=self.freq,
+        	callback=self.set_freq,
+        	label="frequency",
+        	converter=forms.float_converter(),
+        )
+        self.Add(self._freq_text_box)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.GetWin(),
+        	title="Scope Plot",
+        	sample_rate=audio_rate,
+        	v_scale=0,
+        	v_offset=0,
+        	t_scale=0,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=int(samp_rate * 1.0),
+                decimation=audio_rate * audio_interp,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + "" )
+        self.osmosdr_sink_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0.set_center_freq(freq, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(10, 0)
+        self.osmosdr_sink_0.set_if_gain(20, 0)
+        self.osmosdr_sink_0.set_bb_gain(20, 0)
+        self.osmosdr_sink_0.set_antenna("", 0)
+        self.osmosdr_sink_0.set_bandwidth(0, 0)
+          
         _gain_sizer = wx.BoxSizer(wx.VERTICAL)
         self._gain_text_box = forms.text_box(
         	parent=self.GetWin(),
@@ -74,45 +112,7 @@ class Transmit(grc_wxgui.top_block_gui):
         	proportion=1,
         )
         self.Add(_gain_sizer)
-        self._freq_text_box = forms.text_box(
-        	parent=self.GetWin(),
-        	value=self.freq,
-        	callback=self.set_freq,
-        	label="frequency",
-        	converter=forms.float_converter(),
-        )
-        self.Add(self._freq_text_box)
-        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
-        	self.GetWin(),
-        	title="Scope Plot",
-        	sample_rate=audio_rate,
-        	v_scale=0,
-        	v_offset=0,
-        	t_scale=0,
-        	ac_couple=False,
-        	xy_mode=False,
-        	num_inputs=1,
-        	trig_mode=wxgui.TRIG_MODE_AUTO,
-        	y_axis_label="Counts",
-        )
-        self.Add(self.wxgui_scopesink2_0.win)
-        self.uhd_usrp_sink_0 = uhd.usrp_sink(
-        	",".join(("", "")),
-        	uhd.stream_args(
-        		cpu_format="fc32",
-        		channels=range(1),
-        	),
-        )
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_sink_0.set_center_freq(freq, 0)
-        self.uhd_usrp_sink_0.set_gain(gain, 0)
-        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
-                interpolation=int(samp_rate * 1.0),
-                decimation=audio_rate * audio_interp,
-                taps=None,
-                fractional_bw=None,
-        )
-        self.audio_source_0 = audio.source(audio_rate, "hackrf", True)
+        self.audio_source_0 = audio.source(48000, "", True)
         self.analog_nbfm_tx_0 = analog.nbfm_tx(
         	audio_rate=audio_rate,
         	quad_rate=audio_rate * audio_interp,
@@ -126,7 +126,7 @@ class Transmit(grc_wxgui.top_block_gui):
         self.connect((self.analog_nbfm_tx_0, 0), (self.rational_resampler_xxx_0, 0))    
         self.connect((self.audio_source_0, 0), (self.analog_nbfm_tx_0, 0))    
         self.connect((self.audio_source_0, 0), (self.wxgui_scopesink2_0, 0))    
-        self.connect((self.rational_resampler_xxx_0, 0), (self.uhd_usrp_sink_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.osmosdr_sink_0, 0))    
 
 
     def get_samp_rate(self):
@@ -134,7 +134,7 @@ class Transmit(grc_wxgui.top_block_gui):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.uhd_usrp_sink_0.set_samp_rate(self.samp_rate)
+        self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
 
     def get_gain(self):
         return self.gain
@@ -143,7 +143,6 @@ class Transmit(grc_wxgui.top_block_gui):
         self.gain = gain
         self._gain_slider.set_value(self.gain)
         self._gain_text_box.set_value(self.gain)
-        self.uhd_usrp_sink_0.set_gain(self.gain, 0)
 
     def get_freq(self):
         return self.freq
@@ -151,7 +150,7 @@ class Transmit(grc_wxgui.top_block_gui):
     def set_freq(self, freq):
         self.freq = freq
         self._freq_text_box.set_value(self.freq)
-        self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
+        self.osmosdr_sink_0.set_center_freq(self.freq, 0)
 
     def get_audio_rate(self):
         return self.audio_rate
---
+++ rx.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python2
+##################################################
+# GNU Radio Python Flow Graph
+# Title: rx
+# Generated: Sun Sep 27 04:26:53 2015
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from grc_gnuradio import blks2 as grc_blks2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+
+
+class rx(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="rx")
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 48000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_fftsink2_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=0,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=False,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0.win)
+        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.rtlsdr_source_0.set_sample_rate(samp_rate)
+        self.rtlsdr_source_0.set_center_freq(100e6, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(10, 0)
+        self.rtlsdr_source_0.set_if_gain(20, 0)
+        self.rtlsdr_source_0.set_bb_gain(20, 0)
+        self.rtlsdr_source_0.set_antenna("", 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+          
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=500,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, (), 23000, samp_rate)
+        self.digital_gfsk_demod_0 = digital.gfsk_demod(
+        	samples_per_symbol=9,
+        	sensitivity=1.0,
+        	gain_mu=0.175,
+        	mu=0.5,
+        	omega_relative_limit=0.005,
+        	freq_error=0.0,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, "/Users/majora/Documents/output.txt", False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blks2_packet_decoder_0 = grc_blks2.packet_demod_c(grc_blks2.packet_decoder(
+        		access_code="",
+        		threshold=-1,
+        		callback=lambda ok, payload: self.blks2_packet_decoder_0.recv_pkt(ok, payload),
+        	),
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_decoder_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.digital_gfsk_demod_0, 0), (self.blks2_packet_decoder_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.wxgui_fftsink2_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.digital_gfsk_demod_0, 0))    
+        self.connect((self.rtlsdr_source_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))    
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
+
+
+if __name__ == '__main__':
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = rx()
+    tb.Start(True)
+    tb.Wait()
